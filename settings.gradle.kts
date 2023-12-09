75ad34208347bea7052cbc2ffea5c90f1d2ec060
maver <olibgaming10@gmail.com>
2023-12-09 19:29:46 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "JohnOliverFerrer-Block1-Quiz"
+include(":app")
+ 
